a9e103484781a0ed2cf5af02880e4b423d3ba37b
Defne Buse Çelik <110555559+defnebusecelik@users.noreply.github.com>
2023-05-04 21:14:32 +0300

    Add files via upload

---
+++ SQLQuery4.sql
@@ -0,0 +1,17 @@
+create table IRBook(
+id int NOT NULL IDENTITY(1,1) primary key,
+std_enroll varchar(250) not null,
+std_name varchar(250) not null,
+std_dep varchar(250) not null,
+std_sem varchar(250) not null,
+std_contact bigint not null,
+std_email varchar(250) not null,
+book_name varchar(250) not null,
+book_issue_date varchar(250) not null,
+book_return_date varchar(250) not null);
+
+insert into IRBook(std_enroll,std_name,std_dep,std_sem,std_contact,std_email,book_name,book_issue_date,book_return_date) values ('1234567','Defne','Computer Engineering','4.Semester',5000000000,'defne@mail.com','Python','4 Mayýs 2023 Perþembe','10 Mayýs 2023 Çarþamba');
+
+SELECT * FROM IRBook
+
+SELECT * FROM IRBook WHERE std_enroll='1234' and book_return_date IS NULL
